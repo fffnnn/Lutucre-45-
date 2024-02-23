2eca73fc01bb38ff5f6f69d14f78ca728a61d516
fffnnn <159195039+fffnnn@users.noreply.github.com>
2024-02-23 21:13:01 +0500

    answer question no 4

---
+++ nadeem4.js
@@ -0,0 +1,3 @@
+var qoute = "a person who never made a mistake tried anything new";
+var author = "Albret Enistein";
+console.log("".concat(author, ") once said,").concat(qoute));
---
+++ nadeem4.ts
@@ -0,0 +1,3 @@
+let qoute: string= "a person who never made a mistake tried anything new";
+let author: string= "Albret Enistein";
+console.log(`${author}) once said,${qoute}`);
