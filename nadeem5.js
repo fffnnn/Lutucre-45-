fda951d4fe0d2462516ee3ed2e2bf97c46864c65
fffnnn <159195039+fffnnn@users.noreply.github.com>
2024-02-23 21:42:37 +0500

    solved question no 5

---
+++ nadeem5.js
@@ -0,0 +1,4 @@
+var qoute = "a person who never made a mistake tried anything new";
+var famous_person = "Albret Einstien";
+var meesege = "".concat(famous_person, " once said, ").concat(qoute);
+console.log(meesege);
---
+++ nadeem5.ts
@@ -0,0 +1,4 @@
+let qoute: string= "a person who never made a mistake tried anything new";
+let famous_person="Albret Einstien";
+let meesege=`${famous_person} once said, ${qoute}`;
+console.log(meesege);
