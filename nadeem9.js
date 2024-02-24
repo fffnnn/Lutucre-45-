fc08bf1d0c548fd6d94d9a15e3ce75172825039e
fffnnn <159195039+fffnnn@users.noreply.github.com>
2024-02-24 11:21:45 +0500

    solved question 9 answer

---
+++ nadeem9.js
@@ -0,0 +1,2 @@
+var foveriteNumber = 4;
+console.log("my fevorite number is ".concat(foveriteNumber));
---
+++ nadeem9.ts
@@ -0,0 +1,2 @@
+let foveriteNumber: Number = 4;
+console.log(`my fevorite number is ${foveriteNumber}`);
